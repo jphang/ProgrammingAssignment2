90684bd76796d8d1d5e1c6720d3f8abf61ac13c8
Justin Phang <justinjphang@gmail.com>
2015-08-20 23:00:10 +0000

    Commentary added

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,9 @@
 
 ## Write a short comment describing this function
 
+#This is a constructor that contains getters
+#and setters for both the matrix parameter
+#and its inverse by putting the contents in a list.
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -20,6 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+#Assume x is both a square and invertible matrix.
+#This takes the inverse of a matrix x and returns it.
+#First it checks if the inverse was already computed.
+#If so, it simply returns the stored inverse matrix.
+#Otherwise, it gets the contents of the matrix and inverts
+#it via the solve function, stores the inverse (setinverse) 
+#so it can be computed easily in the future, and returns it.
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     i <- x$getinverse()
